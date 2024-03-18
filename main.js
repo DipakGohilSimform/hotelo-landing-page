329b7ae46319f2b669480c66e692b2beff27ddc5
Dipak Gohil <dipak@simformsolutions.com>
2024-03-18 18:35:21 +0530

    landing page completed

---
+++ main.js
@@ -0,0 +1,25 @@
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+const hamburger = () => {
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+};
+
+navToggle.addEventListener("click", hamburger);
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false,
+    },
+  },
+});
